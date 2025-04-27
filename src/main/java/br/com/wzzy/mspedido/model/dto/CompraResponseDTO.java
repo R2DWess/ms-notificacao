ffec0a0047a73c090b28c358d28be228fc212703
R2DWess <gusterlima@hotmail.com>
2025-04-27 01:30:58 -0300

    feat(dto): Added @NoArgsConstructor to CompraResponseDTO

--- src/main/java/br/com/wzzy/mspedido/model/dto/CompraResponseDTO.java
+++ src/main/java/br/com/wzzy/mspedido/model/dto/CompraResponseDTO.java
@@ -2,11 +2,13 @@ package br.com.wzzy.mspedido.model.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CompraResponseDTO {
 
     private String mensagem;
